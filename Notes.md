fbedd81a3fede6911d0e8e2da45634d671f02b14
DOJ <omarjeng999@gmail.com>
2022-04-24 20:38:17 +0000

    MUI Buttons

---
+++ Notes.md
@@ -0,0 +1,20 @@
+# Learning MUI
+
+- After installation,
+- use Roboto Google Fonts, because MUI depends on it.
+
+# Button
+
+```js
+<Button variant="contained" color="secondary" size="small">
+  Contained
+</Button>
+```
+
+- You can add icons on left and right
+
+```js
+<Button startIcon={<Add />} variant="contained" color="secondary" size="small">
+  Add Post
+</Button>
+```
--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Button } from "@mui/material";
+import { Add, SettingsIcon } from "@mui/icons-material";
+
 function App() {
-  return <div>Hello MUI</div>;
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Contained
+      </Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button disabled variant="contained">
+        Disabled Contained
+      </Button>
+    </div>
+  );
 }
 
 export default App;
